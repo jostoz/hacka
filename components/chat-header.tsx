9c1ea64923cc0900a6c83139d6b67952ec40fb99
jostoz <150358190+jostoz@users.noreply.github.com>
2024-11-25 06:08:17 -0600

    X

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,28 +1,55 @@
+'use client';
+
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useWindowSize } from 'usehooks-ts';
+
+import { ModelSelector } from '@/components/model-selector';
+import { SidebarToggle } from '@/components/sidebar-toggle';
+import { Button } from '@/components/ui/button';
+import { BetterTooltip } from '@/components/ui/tooltip';
+import { PlusIcon, VercelIcon } from './icons';
+import { useSidebar } from './ui/sidebar';
+
 export function ChatHeader({ selectedModelId }: { selectedModelId: string }) {
-  const router =useRouter();
- const { open } = useSidebar();
- const { width: windowWidth } = useWindowSize();
+  const router = useRouter();
+  const { open } = useSidebar();
+
+  const { width: windowWidth } = useWindowSize();
+
   return (
-   <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
-     <SidebarToggle />
-     {(!open || windowWidth < 768) && (
-       <BetterTooltip content="New Chat">
-         <Button
-           variant="outline"
-           className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-auto md:ml-0"
-           onClick={() => {
-             router.push('/');
-             router.refresh();
-           }}
-         >
-           <PlusIcon />
-           <span className="md:sr-only">New Chat</span>
-         </Button>
-       </BetterTooltip>
-     )}
-     <ModelSelector
-       selectedModelId={selectedModelId}
-       className="order-1 md:order-2"
-     />
-   </header>
- );
+    <header className="flex sticky top-0 bg-background py-1.5 items-center px-2 md:px-2 gap-2">
+      <SidebarToggle />
+      {(!open || windowWidth < 768) && (
+        <BetterTooltip content="New Chat">
+          <Button
+            variant="outline"
+            className="order-2 md:order-1 md:px-2 px-2 md:h-fit ml-auto md:ml-0"
+            onClick={() => {
+              router.push('/');
+              router.refresh();
+            }}
+          >
+            <PlusIcon />
+            <span className="md:sr-only">New Chat</span>
+          </Button>
+        </BetterTooltip>
+      )}
+      <ModelSelector
+        selectedModelId={selectedModelId}
+        className="order-1 md:order-2"
+      />
+      <Button
+        className="bg-zinc-900 dark:bg-zinc-100 hover:bg-zinc-800 dark:hover:bg-zinc-200 text-zinc-50 dark:text-zinc-900 hidden md:flex py-1.5 px-2 h-fit md:h-[34px] order-4 md:ml-auto"
+        asChild
+        <Link
+          href="https://docs.google.com/presentation/d/1jhfrR_fRazsBLNq4vDa1OaU96nMoqA2ZoVJrbSS0bN4/edit?usp=sharing"
+          target="_blank"
+        >
+          <VercelIcon size={16} />
+          NAVI TUTOR
+        </Link>
+      </Button>
+    </header>
+  );
+}
