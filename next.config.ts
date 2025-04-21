777701cd02c54e6529ac2fa1086a34006ec13e8f
jostoz <jostoztado@gmail.com>
2025-04-21 14:44:10 -0600

    Enhance next.config.ts by adding serverExternalPackages and custom webpack configuration
    - Introduced 'bcrypt-ts' as a server external package to improve security handling.
    - Added a custom webpack configuration section for future enhancements.
    - Updated package.json and pnpm-lock.yaml to include 'bcrypt-ts', 'drizzle-orm', and 'postgres' dependencies for improved functionality.

--- next.config.ts
+++ next.config.ts
@@ -5,12 +5,17 @@ const nextConfig: NextConfig = {
   experimental: {
     ppr: true,
   },
+  serverExternalPackages: ['bcrypt-ts'],
   images: {
     remotePatterns: [
       {
         hostname: 'avatar.vercel.sh',
       },
     ],
+  },
+  webpack: (config, { isServer }) => {
+    // Add any necessary webpack configurations here
+    return config;
   },
 };
 
